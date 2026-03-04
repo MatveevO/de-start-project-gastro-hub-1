813e36065239f74ed66dfc46c2b9a724626eaaca
MatveevO <50818291+MatveevO@users.noreply.github.com>
2026-03-05 02:01:20 +0300

    Update task_4.sql
    05/03/2026 {1}

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,26 @@
 /*добавьте сюда запрос для решения задания 4*/
+
+----------Task 4-------------
+--Найдите пиццерию с самым большим количеством пицц в меню. Если таких пиццерий несколько, выведите все.
+
+with count_pizza_by_cafe as (
+select
+	t.cafe_name,
+	count(t.pizza_name) as count_pizza
+from
+(select distinct r.cafe_name, 
+                jsonb_object_keys(r.menu->'Пицца') as pizza_name
+from cafe.restaurants r
+where r.type in ('pizzeria')) t	
+group by t.cafe_name
+order by count_pizza desc
+)
+select 
+    c.cafe_name,
+	c.count_pizza
+from count_pizza_by_cafe c
+where c.count_pizza in (
+						select 
+							MAX(c.count_pizza)
+						from count_pizza_by_cafe c
+);
