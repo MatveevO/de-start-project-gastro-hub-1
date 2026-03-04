3c0febb4c4033990b571279f7254af4d1c1a2fd3
MatveevO <50818291+MatveevO@users.noreply.github.com>
2026-03-05 02:01:45 +0300

    Update task_5.sql
    05/03/2026 {1}

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,26 @@
 /*добавьте сюда запрос для решения задания 5*/
+----------Task 5-------------
+--Найдите самую дорогую пиццу для каждой пиццерии.
+
+with cost_pizza_by_cafe as (
+select
+	t.cafe_name,
+	t.pizza_name,
+	t.pizza_cost,
+	ROW_NUMBER() over (partition by t.cafe_name order by t.pizza_cost DESC) as rn
+from
+(select distinct r.cafe_name,
+                jsonb_object_keys(r.menu->'Пицца') as pizza_name,
+                r.menu-> 'Пицца' -> jsonb_object_keys(r.menu->'Пицца') as pizza_cost
+from cafe.restaurants r
+where r.type in ('pizzeria')) t	
+order by t.cafe_name, t.pizza_cost desc
+)
+select 
+    c.cafe_name,
+    'Пицца' type,
+	c.pizza_name,
+	c.pizza_cost
+from cost_pizza_by_cafe c
+where c.rn = 1
+;
