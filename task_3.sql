9ea2946a12a078a217d736f70b1fe6c723ece52c
MatveevO <50818291+MatveevO@users.noreply.github.com>
2026-03-05 01:58:51 +0300

    Update task_3.sql
    05/03/2026 {1}

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,12 @@
 /*добавьте сюда запрос для решения задания 3*/
+----------Task 3-------------
+--Найдите топ-3 заведения, где чаще всего менялся менеджер за весь период.
+
+select distinct 
+	r.cafe_name, 
+	count(w.manager_uuid) over (partition by w.restaurant_uuid) as count_change
+from cafe.restaurant_manager_work_dates w
+left join cafe.restaurants r using(restaurant_uuid)
+order by count_change desc
+limit 3
+;
